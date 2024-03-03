de5442731e24d63008ff7fc7f27be2dd8538d91e
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-02 18:05:07 -0600

    add: spec for rendering all articles

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'user sees all article', type: :feature do
   describe 'they visit /articles' do
     scenario 'displays all articles' do
-      article_1 = Artilcle.create!(title: 'title 1', body: 'body 1')
-      article_2 = Artilcle.create!(title: 'title 2', body: 'body 2')
+      article_1 = Article.create!(title: 'title 1', body: 'body 1')
+      article_2 = Article.create!(title: 'title 2', body: 'body 2')
 
       visit '/articles'
 
