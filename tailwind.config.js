1a3b4623f4870c12e40348d861c57f1ce795ce09
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-08 12:26:53 -0600

    feature: slide over feature not working yet

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.{html,erb}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{js,jsx,ts,tsx,vue}",
+    // Add any other paths that contain Tailwind class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
