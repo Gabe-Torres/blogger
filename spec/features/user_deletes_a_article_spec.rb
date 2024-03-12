ab62fd1b45d9221246c7db32e6609709b52d1e4a
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-11 20:17:12 -0500

    feature: user deletes an article

--- spec/features/user_deletes_a_article_spec.rb
+++ spec/features/user_deletes_a_article_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "user deletes a article", type: :feature do
       click_link "Delete"
 
       expect(current_path).to eq(articles_path)
-      expect(page).to have_content(article1.title)
+      expect(page).to_not have_content(article1.title)
       expect(page).to have_content(article2.title)
     end
   end
