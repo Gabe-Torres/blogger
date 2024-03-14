691ca2b60affdb79d65985e82aabf65b89854808
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-14 00:53:33 -0500

    update: test to include comments

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "user sees one article", type: :feature do
   describe "they link from the articles index" do
     it "displays information for one article" do
       article = Article.create!(title: "New title", body: "new body")
+      comment1 = article.comments.create(author_name: "dude", body: "yessssiirrrr")
+      comment2 = article.comments.create(author_name: "dudie", body: "bruuuuuu")
 
       visit articles_path
 
@@ -11,6 +13,10 @@ RSpec.describe "user sees one article", type: :feature do
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+      expect(page).to have_content(comment1.author_name)
+      expect(page).to have_content(comment1.body)
+      expect(page).to have_content(comment2.author_name)
+      expect(page).to have_content(comment2.body)
     end
   end
 end
