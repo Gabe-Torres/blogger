27f7e62d9f57f1c46f3955adbbdfc4ea406e4f1d
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-15 16:12:22 -0500

    schema: create tagging model and specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe 'Associations' do
-    it {should have_many(:comments)}
+    it { should have_many(:comments) }
+    it { should have_many(:taggings) }
+    it { should have_many(:tags).through(:taggings) }
   end
 
   describe 'Validations' do
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  describe 'Associations' do
+    it { should have_many(:taggings) }
+    it { should have_many(:articles).through(:taggings) }
+  end
+
+  describe 'Validations' do
+    # it { should validate_presence_of :example }
+  end
+
+  describe 'Instance Methods' do
+    
+  end
+end
