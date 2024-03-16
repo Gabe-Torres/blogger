d0a4841804fb1408bb36efe8c55988078d7ea5fd
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-15 21:00:12 -0500

    feature: a user can create tags and link to tag show

--- spec/features/user_creates_new_article_spec.rb
+++ spec/features/user_creates_new_article_spec.rb
@@ -12,13 +12,14 @@ RSpec.describe 'user creates new article', type: :feature do
 
         fill_in 'Title', with: 'new title'
         fill_in 'Body', with: 'new body'
+        fill_in "article[tag_list]", with: "ruby technology"
 
         click_on 'Create Article'
 
         expect(page).to have_content('new title')
         expect(page).to have_content('new body')
         expect(page).to have_content('Article created!')
-
+        expect(page).to have_content('ruby technology')
       end
     end
   end
