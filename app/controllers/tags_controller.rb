c9525c50174c3c9ac16bc03212f6ae4aa3f45f12
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-26 23:48:56 -0500

    feature: a user can delete a single tag from an article

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,14 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+
+    @tag.destroy
+
+    flash.notice = 'Tag deleted!'
+
+    redirect_to articles_path
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :taggings
-  has_many :tags, through: :taggings 
+  has_many :tags, through: :taggings
   has_many :comments, dependent: :destroy
 
   validates_presence_of :title,
