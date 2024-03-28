ee394e665dfd821175cf94853f2322442be71ecd
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-27 22:32:56 -0500

    add: images to article object and params

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
   has_many :comments, dependent: :destroy
+  has_many_attached :images
 
   validates_presence_of :title,
                         :body
