89f5b713fd48a1e2d4f627f310cdb1a6e671890f
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-28 13:26:21 -0500

    feature: a user can log in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show destroy edit update]
+  before_action :authenticate_user!
 
   def index
     @articles = Article.all
