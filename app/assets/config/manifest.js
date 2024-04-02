48bb66a5941c5d2bb17f05d20dbd8cb3de76d865
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-04-01 22:23:41 -0500

    fix bootstrap and tailwind.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,11 @@
 //= link_tree ../images
- //= link popper.js
 //= link_tree ../builds
 //= tailwindcss/rails/application.css
+//= link controllers/application.js
+//= link controllers/focus_controller.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link controllers/remove_controller.js
+//= link controllers/slide_over_controller.js
+//= link bootstrap.js
+//= link @popperjs--core.js
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.entry = {
+  application: './app/javascript/application.js',
+}
+
+// const webpack = require('webpack')
+// environment.plugins.append('Provide', new webpack.ProvidePlugin({
+//   $: 'jquery',
+//   jQuery: 'jquery',
+//   Popper: ['popper.js', 'default']
+// }))
+
+
 module.exports = environment
