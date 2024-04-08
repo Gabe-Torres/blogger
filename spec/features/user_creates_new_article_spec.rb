afdb67364b6d5f500fb3c32c1257915e3d043470
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-04-07 23:46:13 -0500

    edit happy path test outline

--- spec/features/user_creates_new_article_spec.rb
+++ spec/features/user_creates_new_article_spec.rb
@@ -3,26 +3,28 @@ require 'rails_helper'
 RSpec.describe 'user creates new article', type: :feature do
   describe 'they link from the article index' do
     describe 'they fill in the form' do
-      it 'creates a new article' do
-        user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
-        login_as(user, :scope => :user)
+      describe 'Happy Path, Success 200' do
+        scenario 'creates a new article' do
+          user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
+          login_as(user, :scope => :user)
 
-        visit articles_path
+          visit articles_path
 
-        click_link 'New Article'
+          click_link 'New Article'
 
-        expect(current_path).to eq(new_article_path)
+          expect(current_path).to eq(new_article_path)
 
-        fill_in 'Title', with: 'new title'
-        fill_in 'Body', with: 'new body'
-        fill_in "article[tag_list]", with: "ruby technology"
+          fill_in 'Title', with: 'new title'
+          fill_in 'Body', with: 'new body'
+          fill_in "article[tag_list]", with: "ruby technology"
 
-        click_on 'Create Article'
+          click_on 'Create Article'
 
-        expect(page).to have_content('new title')
-        expect(page).to have_content('new body')
-        expect(page).to have_content('Article created!')
-        expect(page).to have_content('ruby technology')
+          expect(page).to have_content('new title')
+          expect(page).to have_content('new body')
+          expect(page).to have_content('Article created!')
+          expect(page).to have_content('ruby technology')
+        end
       end
     end
   end
--- spec/features/user_edits_a_article_spec.rb
+++ spec/features/user_edits_a_article_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'user edits one article', type: :feature do
   describe 'they link from the articles show' do
-    it 'displays a form to edit an article' do
-      user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
-      login_as(user, :scope => :user)
+    describe 'Happy Path, Success 200' do
+      scenario 'displays a form to edit an article with valid input' do
+        user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
+        login_as(user, :scope => :user)
 
-      article = user.articles.create!(title: 'New title', body: 'new body')
+        article = user.articles.create!(title: 'New title', body: 'new body')
 
-      visit article_path(article)
+        visit article_path(article)
 
-      click_link 'Edit'
+        click_link 'Edit'
 
-      expect(current_path).to eq(edit_article_path(article))
+        expect(current_path).to eq(edit_article_path(article))
 
-      fill_in 'Body', with: 'different'
+        fill_in 'Body', with: 'different'
 
-      click_on 'Update Article'
+        click_on 'Update Article'
 
-      expect(page).to have_content('different')
-      expect(page).to_not have_content('new body')
-      expect(page).to have_content("Article '#{article.title}' updated!")
+        expect(page).to have_content('different')
+        expect(page).to_not have_content('new body')
+        expect(page).to have_content("Article '#{article.title}' updated!")
+      end
     end
   end
 end
