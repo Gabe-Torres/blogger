2d82607f7b5a16fb407689e998634cd14d0264ae
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-05-10 23:17:06 -0500

    adds sad path testing for creating article with invalid entries

--- spec/features/user_creates_new_article_spec.rb
+++ spec/features/user_creates_new_article_spec.rb
@@ -26,6 +26,41 @@ RSpec.describe 'user creates new article', type: :feature do
           expect(page).to have_content('ruby technology')
         end
       end
+
+      describe "Sad Path, Error 422" do
+        scenario 'creates a new article with invalid entries' do
+        user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
+        login_as(user, :scope => :user)
+
+        visit articles_path
+
+        click_link 'New Article'
+
+        expect(current_path).to eq(new_article_path)
+
+        fill_in 'Title', with: 'new title'
+        fill_in 'Body', with: ''
+        fill_in "article[tag_list]", with: "ruby technology"
+
+        click_on 'Create Article'
+        expect(page).to have_content("Body can't be blank")
+
+        fill_in 'Title', with: ''
+        fill_in 'Body', with: 'new title'
+        fill_in "article[tag_list]", with: "ruby technology"
+
+        click_on 'Create Article'
+        expect(page).to have_content("Title can't be blank")
+
+        fill_in 'Title', with: ''
+        fill_in 'Body', with: ''
+        fill_in "article[tag_list]", with: "ruby technology"
+
+        click_on 'Create Article'
+        expect(page).to have_content("Title can't be blank")
+        expect(page).to have_content("Body can't be blank")
+        end
+      end
     end
   end
 end
