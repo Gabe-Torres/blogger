3ebb42e4065573c6ace8c9cf515a5b2e30fb6c1c
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-05-13 20:29:47 -0500

    create factories and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+include FactoryBot::Syntax::Methods
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Article.destroy_all
+Comment.destroy_all
+Tag.destroy_all
+Tagging.destroy_all
+
+5.times { create(:tag) }
+
+10.times do
+  user = FactoryBot.create(:user)
+  5.times do
+    article = FactoryBot.create(:article, user: user)
+
+    # Create comments for each article
+    rand(1..4).times do
+      FactoryBot.create(:comment, article: article)
+    end
+
+    # Create tags and taggings for each article
+    article.tags << Tag.all.sample(rand(1..3))
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 10, max_length: 20, mix_case: true) } # You can use Faker here too if you want
+  end
+end
